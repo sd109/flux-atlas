c68aa8c3fc479e3eb857e9e5dc29f77cc97ade49
Scott Davidson <ilikeducks546@gmail.com>
2024-08-30 21:13:36 +0100

    Use fairing for CORS response header and catch k8s API errors

--- backend/src/fluxcd/utils.rs
+++ backend/src/fluxcd/utils.rs
@@ -2,32 +2,36 @@ use kube::{
     api::{Api, ListParams},
     Client, Resource,
 };
-use rocket::serde::DeserializeOwned;
+use rocket::{http::Status, serde::DeserializeOwned};
 use std::fmt::Debug;
 
-async fn list_resources<K: Resource + Debug + DeserializeOwned + Clone>(client: &Client) -> Vec<K>
+async fn list_resources<K: Resource + Debug + DeserializeOwned + Clone>(
+    client: &Client,
+) -> Result<Vec<K>, Status>
 where
     <K as Resource>::DynamicType: Default,
 {
     // NOTE: It looks like client clones are cheap, so using to_owned here is fine
     // https://github.com/kube-rs/kube/blob/0.93.1/kube-client/src/client/mod.rs#L76-L81
-    Api::<K>::all(client.to_owned())
+    match Api::<K>::all(client.to_owned())
         .list(&ListParams::default())
         .await
-        .unwrap() // TODO: Handle errors here
-        .items
+    {
+        Ok(response) => Ok(response.items),
+        Err(_) => Err(Status::ServiceUnavailable),
+    }
 }
 
 pub async fn fetch_view<K: Debug + Clone + kube::Resource + DeserializeOwned, V>(
     client: &Client,
-) -> Vec<V>
+) -> Result<Vec<V>, Status>
 where
     V: From<K>,
     <K as kube::Resource>::DynamicType: Default,
 {
-    list_resources(client)
-        .await
+    Ok(list_resources(client)
+        .await?
         .into_iter()
         .map(|hr: K| V::from(hr))
-        .collect()
+        .collect())
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -12,7 +12,12 @@ use kube_custom_resources_rs::{
         v1beta2::ocirepositories::OCIRepository,
     },
 };
-use rocket::{http::Header, serde::json::Json, State};
+use rocket::{
+    fairing::AdHoc,
+    http::Status,
+    serde::{json::Json, Serialize},
+    Request, State,
+};
 
 mod fluxcd;
 use fluxcd::{
@@ -20,68 +25,77 @@ use fluxcd::{
     OCIRepoView,
 };
 
-#[derive(Responder)]
-#[response(content_type = "json")]
-struct CorsResponder<T> {
-    inner: Json<T>,
-    cors: Header<'static>,
-}
-
-impl<T> CorsResponder<T> {
-    fn new(inner: Json<T>, allowed_origins: &str) -> Self {
-        CorsResponder {
-            inner,
-            cors: Header::new("Access-Control-Allow-Origin", allowed_origins.to_owned()),
-        }
-    }
-}
-
 #[get("/git-repos")]
-async fn git_repos(client: &State<Client>) -> CorsResponder<Vec<GitRepoView>> {
-    CorsResponder::new(
-        Json(fetch_view::<GitRepository, GitRepoView>(client).await),
-        "*",
+async fn git_repos(client: &State<Client>) -> Json<Vec<GitRepoView>> {
+    Json(
+        fetch_view::<GitRepository, GitRepoView>(client)
+            .await
+            .unwrap(),
     )
 }
 
 #[get("/oci-repos")]
-async fn oci_repos(client: &State<Client>) -> CorsResponder<Vec<OCIRepoView>> {
-    CorsResponder::new(
-        Json(fetch_view::<OCIRepository, OCIRepoView>(client).await),
-        "*",
+async fn oci_repos(client: &State<Client>) -> Json<Vec<OCIRepoView>> {
+    Json(
+        fetch_view::<OCIRepository, OCIRepoView>(client)
+            .await
+            .unwrap(),
     )
 }
 
 #[get("/helm-repos")]
-async fn helm_repos(client: &State<Client>) -> CorsResponder<Vec<HelmRepoView>> {
-    CorsResponder::new(
-        Json(fetch_view::<HelmRepository, HelmRepoView>(client).await),
-        "*",
+async fn helm_repos(client: &State<Client>) -> Json<Vec<HelmRepoView>> {
+    Json(
+        fetch_view::<HelmRepository, HelmRepoView>(client)
+            .await
+            .unwrap(),
     )
 }
 
 #[get("/helm-charts")]
-async fn helm_charts(client: &State<Client>) -> CorsResponder<Vec<HelmChartView>> {
-    CorsResponder::new(
-        Json(fetch_view::<HelmChart, HelmChartView>(client).await),
-        "*",
+async fn helm_charts(client: &State<Client>) -> Json<Vec<HelmChartView>> {
+    Json(
+        fetch_view::<HelmChart, HelmChartView>(client)
+            .await
+            .unwrap(),
     )
 }
 
 #[get("/helm-releases")]
-async fn helm_releases(client: &State<Client>) -> CorsResponder<Vec<HelmReleaseView>> {
-    CorsResponder::new(
-        Json(fetch_view::<HelmRelease, HelmReleaseView>(client).await),
-        "*",
+async fn helm_releases(client: &State<Client>) -> Json<Vec<HelmReleaseView>> {
+    Json(
+        fetch_view::<HelmRelease, HelmReleaseView>(client)
+            .await
+            .unwrap(),
     )
 }
 
 #[get("/kustomizations")]
-async fn kustomizations(client: &State<Client>) -> CorsResponder<Vec<KustomizationView>> {
-    CorsResponder::new(
-        Json(fetch_view::<Kustomization, KustomizationView>(client).await),
-        "*",
-    )
+async fn kustomizations(client: &State<Client>) -> Result<Json<Vec<KustomizationView>>, Status> {
+    let response = fetch_view::<Kustomization, KustomizationView>(client).await?;
+    Ok(Json(response))
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct ErrorResponse {
+    error: String,
+}
+
+impl ErrorResponse {
+    fn new<S: Into<String>>(error: S) -> Self {
+        ErrorResponse {
+            error: error.into(),
+        }
+    }
+}
+
+#[catch(503)]
+fn handle_503_error<'a>(req: &Request) -> Json<ErrorResponse> {
+    Json(ErrorResponse::new(format!(
+        "endpoint {} currently unavailable",
+        req.uri()
+    )))
 }
 
 #[launch]
@@ -91,15 +105,26 @@ async fn rocket() -> _ {
         .await
         .expect("Kubernetes client to be inferrable from runtime environment");
 
-    rocket::build().manage(client).mount(
-        "/api/",
-        routes![
-            helm_repos,
-            helm_charts,
-            helm_releases,
-            kustomizations,
-            git_repos,
-            oci_repos
-        ],
-    )
+    rocket::build()
+        .mount(
+            "/api/",
+            routes![
+                helm_repos,
+                helm_charts,
+                helm_releases,
+                kustomizations,
+                git_repos,
+                oci_repos
+            ],
+        )
+        .register("/api/", catchers![handle_503_error])
+        // Add shared state accessible from handlers
+        .manage(client)
+        // Remove CORS restrictions from all responses
+        .attach(AdHoc::on_response("CORS Header", |_, response| {
+            Box::pin(async move {
+                let cors_header = rocket::http::Header::new("Access-Control-Allow-Origin", "*");
+                response.set_header(cors_header);
+            })
+        }))
 }
